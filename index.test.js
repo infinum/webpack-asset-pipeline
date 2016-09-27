b1b852e3d0aafd090bbd231adc6b802ece53ff3b
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2016-09-27 09:25:44 +0200

    test: Add tests for features
    Tests are not yet completed.

---
+++ index.test.js
@@ -0,0 +1,14 @@
+test('it can be required', () => {
+});
+
+test('it emits a file called manifest.json', () => {
+});
+
+test('it can specify the file name of emitted file', () => {
+});
+
+test('it can specify extraneous files', () => {
+});
+
+test('it writes out a file if specified', () => {
+});
