064541164599628accd75d916e71882b547794fe
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2016-11-18 20:37:49 +0100

    feat(example): add code splitting and assets

---
+++ example/files.js
@@ -0,0 +1,1 @@
+require('./logo.svg');
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,35 +1,23 @@
 const path = require('path');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
 const RailsManifestPlugin = require('../');
 
-const port = 8080;
-
-const config = {
-  entry: path.resolve('application.js'),
+module.exports = {
+  entry: {
+    application: path.resolve('application.js'),
+    vendor: path.resolve('vendor.js')
+  },
   output: {
     path: path.resolve('build'),
     filename: '[name].js',
     publicPath: './'
   },
   plugins: [
-    new RailsManifestPlugin({
-      writeToFileEmit: true
-    })
-  ]
+    new RailsManifestPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.svg$/,
+      loader: 'file-loader?name=[hash].[ext]'
+    }]
+  }
 };
-
-new WebpackDevServer(webpack(config), Object.assign({
-  contentBase: path.join(__dirname, 'build'),
-  inline: true,
-  progress: true,
-  compress: true,
-  stats: {
-    colors: true,
-    hash: true,
-    timings: true,
-    chunks: false
-  },
-  historyApiFallback: true
-}))
-.listen(port, '0.0.0.0');
